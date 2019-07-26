3a128312ca8f2b94676835a5e6568e436928439c
SHUBHAM AGARWAL <>
2019-07-26 20:00:27 +0530

    First Commit

---
+++ DropDownSelection/DropDownSelection/ViewController.swift
@@ -0,0 +1,94 @@
+//
+//  ViewController.swift
+//  DropDownSelection
+//
+//  Created by SHUBHAM AGARWAL on 26/07/19.
+//  Copyright © 2019 SHUBHAM AGARWAL. All rights reserved.
+//
+
+import UIKit
+
+class CellClass: UITableViewCell {
+    
+}
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var btnSelectFruit: UIButton!
+    @IBOutlet weak var btnSelectGender: UIButton!
+    
+    let transparentView = UIView()
+    let tableView = UITableView()
+    
+    var selectedButton = UIButton()
+    
+    var dataSource = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(CellClass.self, forCellReuseIdentifier: "Cell")
+    }
+    
+    func addTransparentView(frames: CGRect) {
+        let window = UIApplication.shared.keyWindow
+        transparentView.frame = window?.frame ?? self.view.frame
+        self.view.addSubview(transparentView)
+        
+        tableView.frame = CGRect(x: frames.origin.x, y: frames.origin.y + frames.height, width: frames.width, height: 0)
+        self.view.addSubview(tableView)
+        tableView.layer.cornerRadius = 5
+        
+        transparentView.backgroundColor = UIColor.black.withAlphaComponent(0.9)
+        tableView.reloadData()
+        let tapgesture = UITapGestureRecognizer(target: self, action: #selector(removeTransparentView))
+        transparentView.addGestureRecognizer(tapgesture)
+        transparentView.alpha = 0
+        UIView.animate(withDuration: 0.4, delay: 0.0, usingSpringWithDamping: 1.0, initialSpringVelocity: 1.0, options: .curveEaseInOut, animations: {
+            self.transparentView.alpha = 0.5
+            self.tableView.frame = CGRect(x: frames.origin.x, y: frames.origin.y + frames.height + 5, width: frames.width, height: CGFloat(self.dataSource.count * 50))
+        }, completion: nil)
+    }
+    
+    @objc func removeTransparentView() {
+        let frames = selectedButton.frame
+        UIView.animate(withDuration: 0.4, delay: 0.0, usingSpringWithDamping: 1.0, initialSpringVelocity: 1.0, options: .curveEaseInOut, animations: {
+            self.transparentView.alpha = 0
+            self.tableView.frame = CGRect(x: frames.origin.x, y: frames.origin.y + frames.height, width: frames.width, height: 0)
+        }, completion: nil)
+    }
+
+    @IBAction func onClickSelectFruit(_ sender: Any) {
+        dataSource = ["Apple", "Mango", "Orange"]
+        selectedButton = btnSelectFruit
+        addTransparentView(frames: btnSelectFruit.frame)
+    }
+    
+    @IBAction func onClickSelectGender(_ sender: Any) {
+        dataSource = ["Male", "Female"]
+        selectedButton = btnSelectGender
+        addTransparentView(frames: btnSelectGender.frame)
+    }
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataSource.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = dataSource[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedButton.setTitle(dataSource[indexPath.row], for: .normal)
+        removeTransparentView()
+    }
+}
